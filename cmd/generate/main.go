a15e2d80ed72e168b59a115d738b55de0d98a6d1
Linus Wahlgren <snabelost3@gmail.com>
2021-08-30 13:00:15 +0200

    Added menu and template

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"os"
+	"path"
+
+	"github.com/gomarkdown/markdown"
 )
 
 func main() {
-	template, err := template.ParseFiles("src/template.html")
+	tmpl, err := template.ParseFiles("src/template.html")
 	if err != nil {
 		log.Fatal("template_parse_fail", err)
 	}
@@ -15,12 +20,43 @@ func main() {
 	if err != nil {
 		log.Fatal("ouput_file_open_fail", err)
 	}
-	if err := template.Execute(out, Params{Menu: "derp", Content: "Dorp"}); err != nil {
+	files, err := ioutil.ReadDir("./src/pages")
+	if err != nil {
+		log.Fatal("pages_dir_fail", err)
+	}
+	menu := []MenuItem{}
+	menujs, err := ioutil.ReadFile("./src/menu.json")
+	if err != nil {
+		log.Fatal("menu_read_fail", err)
+	}
+	if err := json.Unmarshal(menujs, &menu); err != nil {
+		log.Fatal("menu_parse_fail", err)
+	}
+	pages := []Page{}
+	for _, f := range files {
+		md, err := ioutil.ReadFile(path.Join("./src/pages", f.Name()))
+		if err != nil {
+			log.Fatal("page_read_fail", path.Join("./src/pages", f.Name()), err)
+		}
+		html := markdown.ToHTML(md, nil, nil)
+		pages = append(pages, Page{ID: f.Name(), HTML: template.HTML(html)})
+	}
+	if err := tmpl.Execute(out, Site{Menu: menu, Pages: pages}); err != nil {
 		log.Fatal("template_execute_fail", err)
 	}
 }
 
-type Params struct {
-	Menu    string
-	Content string
+type Site struct {
+	Menu  []MenuItem
+	Pages []Page
+}
+
+type MenuItem struct {
+	Text string
+	Path string
+}
+
+type Page struct {
+	ID   string
+	HTML template.HTML
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ipx.live/website
 
 go 1.16
+
+require github.com/gomarkdown/markdown v0.0.0-20210820032736-385812cbea76 // indirect
